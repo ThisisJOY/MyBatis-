c91b4df201b2bb9464fbb208d16f64d2a2836ecf
Li <joyinbritish@me.com>
2020-03-02 16:23:06 +0100

    add test module

---
+++ IPersistence_test/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+Y
+)src/main/java/com/lagou/dao/IUserDao.java,9/4/949996937de0fd6a3017001ab391b19968131f00
+V
+&src/main/java/com/lagou/pojo/User.java,1/4/144dfd552d1f8baa2afc272de0c11bafa53676e5
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+b
+2src/main/java/com/lagou/test/IPersistenceTest.java,f/4/f4a5ea11b94550321d5d366061854f9d360902ab
---
+++ IPersistence_test/src/main/java/com/lagou/test/IPersistenceTest.java
@@ -0,0 +1,54 @@
+package com.lagou.test;
+
+import com.lagou.dao.IUserDao;
+import com.lagou.io.Resources;
+import com.lagou.pojo.User;
+import com.lagou.sqlSession.SqlSession;
+import com.lagou.sqlSession.SqlSessionFactory;
+import com.lagou.sqlSession.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class IPersistenceTest {
+
+    @Test
+    public void test() throws Exception {
+        InputStream resourceAsSteam = Resources.getResourceAsSteam("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsSteam);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //调用
+        User user = new User();
+        user.setId(1);
+        user.setUsername("张三");
+      /*  User user2 = sqlSession.selectOne("user.selectOne", user);
+
+        System.out.println(user2);*/
+
+       /* List<User> users = sqlSession.selectList("user.selectList");
+        for (User user1 : users) {
+            System.out.println(user1);
+        }*/
+
+        IUserDao userDao = sqlSession.getMapper(IUserDao.class);
+
+//        List<User> all = userDao.findAll();
+//        for (User user1 : all) {
+//            System.out.println(user1);
+//        }
+
+        User userUpdate = new User();
+        userUpdate.setId(1);
+        userUpdate.setUsername("小明");
+
+        userDao.update(userUpdate);
+
+//        userDao.delete(user);
+
+
+    }
+
+
+
+}
